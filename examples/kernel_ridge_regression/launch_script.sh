752b418c6c25d64ec253086242d234c49ad59dd3
Michael Arbel <marbel@inrialpes.fr>
2022-09-28 17:51:24 +0200

    krr

--- examples/kernel_ridge_regression/launch_script.sh
+++ examples/kernel_ridge_regression/launch_script.sh
@@ -4,12 +4,13 @@ seed=0
 log_name=test
 isBatchJob=True
 
-HYDRA_FULL_ERROR=1   OC_CAUSE=1 python -m ipdb main.py --multirun  \
+HYDRA_FULL_ERROR=1   OC_CAUSE=1 python -m ipdb main.py  \
                 optimizer=sgd,adam\
                 ++logs.log_name=$log_name\
                 ++system.isBatchJob=$isBatchJob\
                 ++system.seed=0,1,2,3\
-                ++max_iter=100\ 
+                ++max_iter=100\
+                ++system.isForceGitClean=False\
 
 
 
