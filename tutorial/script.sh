c01bc8dd1a276eed455358dd8046a1f08eca1640
Michael Arbel <marbel@inrialpes.fr>
2023-04-21 21:21:49 +0200

    mlxpy: Automatically committing all changes

--- tutorial/script.sh
+++ tutorial/script.sh
@@ -2,6 +2,8 @@
 
 
 HYDRA_FULL_ERROR=1   OC_CAUSE=1 python -m ipdb main.py  \
-                +mlxpy.use_scheduler=False\
+                optimizer.lr=10.,1.,0.1\
+                seed=1,2,3,4\
+                +mlxpy.use_scheduler=True\
                 +mlxpy.use_version_manager=True\
                 +mlxpy.use_logger=True\
