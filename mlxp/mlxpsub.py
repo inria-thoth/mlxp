963c85d9dd3153cdb692ab22c1fd12467a6d0f51
Michael Arbel <michael.n.arbel@gmail.com>
2024-12-02 16:58:35 +0100

    deleting variable from environment

--- mlxp/mlxpsub.py
+++ mlxp/mlxpsub.py
@@ -111,8 +111,7 @@ def run_python_script(bash_cmd, bash_script_name, scheduler_file_name):
         sys.stderr.write("{0}: command not found\n".format(bash_cmd))
     else:
         print(code)
-
-    # print(process_output)
+    del os.environ[scheduler_env_var]
 
 
 def mlxpsub():
